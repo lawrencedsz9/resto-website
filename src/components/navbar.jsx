8bc8ad5781c8ff99e84d56025b73d006a68c367e
lawrencedsz9 <lawrencedsouza125@gmail.com>
2023-12-08 19:01:12 +0530

    changed navbar and removed unnecessary things

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -15,6 +15,7 @@ const Navbar = () => {
             Foodtime
           </span>
         </a>
+        {/* Button for mobile view */}
         <button
           onClick={toggleDropdown}
           type="button"
@@ -39,12 +40,9 @@ const Navbar = () => {
             />
           </svg>
         </button>
-        <div
-          className={`hidden w-full md:block md:w-auto ${
-            isDropdownOpen ? "block" : "hidden"
-          }`}
-          id="navbar-dropdown"
-        >
+
+        {/* Updated Navbar Links */}
+        <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a
@@ -55,76 +53,13 @@ const Navbar = () => {
                 Home
               </a>
             </li>
-            <li>
-              <button
-                id="dropdownNavbarLink"
-                onClick={toggleDropdown}
-                className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
-                aria-expanded={isDropdownOpen}
-                aria-controls="dropdownNavbar"
-              >
-                More
-                <svg
-                  className="w-2.5 h-2.5 ms-2.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m1 1 4 4 4-4"
-                  />
-                </svg>
-              </button>
-              {/* Dropdown menu */}
-              <div
-                id="dropdownNavbar"
-                className={`z-10 ${
-                  isDropdownOpen ? "block" : "hidden"
-                } font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}
-              >
-                <ul
-                  className="py-2 text-sm text-gray-700 dark:text-gray-400"
-                  aria-labelledby="dropdownLargeButton"
-                >
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Dashboard
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      About
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-
+            {/* Cart link replacing "offers" */}
             <li>
               <a
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                offers
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
+                Cart
               </a>
             </li>
           </ul>
