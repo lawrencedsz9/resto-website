f26d1a57cd57a98d844094aed508e8468a6e408a
vyshnav <vyshnavravi599@gmail.com>
2023-12-08 19:23:57 +0530

    Refactor Displaycard component and add image and button

--- src/components/Displaycard.jsx
+++ src/components/Displaycard.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from "react";
+import Food from '../../public/food.jpg'
+import { CgAdd } from "react-icons/cg";
+import { FaMinus } from "react-icons/fa";
+import { FiMinusCircle } from "react-icons/fi";
 
-const Displaycard = () => {
-  return (
-      <>
-          <div className='flex justify-start h-screen w-screen items-center'>
-              <div className='flex'>
-              <div className='bg-slate-500 h-80 w-64 rounded-3xl flex justify-center '>
-                  <div className='bg-black w-48 h-48 rounded-full mt-[-80px]'>
-                      
-                  </div>
-              </div>    
-              </div>
-              
-              
-          </div>
-      </>
-  )
-}
 
-export default Displaycard
+const Displaycard = (props) => {
+    return (
+        <>
+            <div className="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden m-4">
+      <img className="w-full h-48 object-cover object-center" src={Food} alt={name} />
+      <div className="p-4">
+        <h2 className="text-gray-900 font-bold text-xl mb-2">{props.name}</h2>
+        <p className="text-gray-700 font-semibold text-lg mb-2">${props.price}</p>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Add to Cart
+        </button>
+      </div>
+    </div>
+        </>
+    );
+};
+
+export default Displaycard;
