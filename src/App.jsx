c1b0a0eede92ec83d4249cfe46f7167827263171
pranavvraja <pranavvraja@gmail.com>
2023-12-08 19:48:13 +0530

    added chat page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AdminPage from "./pages/admin";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import AddItem from "./pages/add";
+import Chat from "./pages/chat";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,7 +17,8 @@ function App() {
       <div className="flex items-center justify-center w-full h-full bg-slate-300">
         <Router>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Chat />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/admin" element={<AdminPage />} />
             <Route path="/add" element={<AddItem />} />
           </Routes>
---
+++ src/pages/chat.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Chat() {
+  return (
+    <div>
+      <h1>Chat Page</h1>
+      {/* Add your chat components and logic here */}
+    </div>
+  );
+}
+
+export default Chat;
