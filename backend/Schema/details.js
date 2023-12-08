9f64bd4bc4b043a4bbe356a2466e399dd62193e2
pranavvraja <pranavvraja@gmail.com>
2023-12-08 19:26:06 +0530

    send items from add page to db

--- backend/Schema/details.js
+++ backend/Schema/details.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const foodSchema = new mongoose.Schema({
-    f_name: String,
-    f_price: Number,
-    best:Boolean
-})
+  f_name: String,
+  f_price: Number,
+  best: Boolean,
+  nonveg: Boolean,
+});
 
-module.exports = mongoose.model('resto-webiste', foodSchema);
+module.exports = mongoose.model("resto-webiste", foodSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
-require('dotenv').config()
-const mongoose = require('mongoose')
-const app = express()
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = express();
 const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-mongoose.connect(process.env.DATA_BASE_URL)
-const foodmodel = require('./Schema/details')
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+mongoose.connect(process.env.DATA_BASE_URL);
+const foodmodel = require("./Schema/details");
+const cors = require("cors");
+app.use(cors());
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -16,24 +17,21 @@ db.once("open", function () {
   console.log("database connected");
 });
 
-app.get('/api/admin/additems', async (req, res) => {
-    const { f_name, f_price } = req.body
+app.post("/add", async (req, res) => {
+  const { item, price, best, nonveg } = req.body;
 
-
-    const data = await foodmodel.insertMany({
-        f_name,
-        f_price,
-        best:true
-    })
-    
-    
-})
+  const data = await foodmodel.insertMany({
+    f_name: item,
+    f_price: price,
+    best: false,
+    nonveg: false,
+  });
+});
 
 app.listen(port, () => {
-    console.log(`app is running in ${port}`)
-})
+  console.log(`app is running in ${port}`);
+});
 
-
-app.get('/', (req, res) => {
-    console.log("user connected")
-})
+app.get("/", (req, res) => {
+  console.log("user connected");
+});
