feda0c0098c388f08d7b1be77e27044e844ce338
“Pranaam19” <22j34.pranam@sjec.ac.in>
2023-12-08 15:17:40 +0530

    homepage

---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <nav>
+                {/* Navbar content */}
+            </nav>
+            <div>
+                {/* Landing page content */}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
