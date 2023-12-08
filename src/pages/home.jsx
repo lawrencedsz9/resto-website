f9a4242fb2733f1cd22456b426602342cee46fbc
“Pranaam19” <22j34.pranam@sjec.ac.in>
2023-12-08 15:18:46 +0530

    hp

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <nav>
-                {/* Navbar content */}
-            </nav>
             <div>
                 {/* Landing page content */}
             </div>
