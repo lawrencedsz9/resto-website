7b2f13c57ae0448cb5d017c129ae9aba3a053bda
pranavvraja <pranavvraja@gmail.com>
2023-12-09 09:53:01 +0530

    welcome_update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -23,63 +23,63 @@ const Footer = () => {
 
   return (
     <footer className="bg-gray-900 p-8 text-center">
-      {" "}
-      // BEGIN: Change background color
       <div className="max-w-2xl mx-auto">
-        <form className="flex flex-col items-center" onSubmit={handleSubmit}>
-          <label
-            htmlFor="name"
-            className="text-left w-full mb-2 text-lg text-gray-700 font-semibold"
-          >
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            placeholder="Your Name"
-            className="input-style"
-            onChange={handleChange}
-          />
+        <div className="card-style">
+          <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+            <label
+              htmlFor="name"
+              className="text-left w-full mb-2 text-lg text-gray-700 font-semibold"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Your Name"
+              className="input-style"
+              onChange={handleChange}
+            />
 
-          <label
-            htmlFor="email"
-            className="text-left w-full mb-2 text-lg text-gray-700 font-semibold"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Your Email"
-            className="input-style"
-            onChange={handleChange}
-          />
+            <label
+              htmlFor="email"
+              className="text-left w-full mb-2 text-lg text-gray-700 font-semibold"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Your Email"
+              className="input-style"
+              onChange={handleChange}
+            />
 
-          <label
-            htmlFor="message"
-            className="text-left w-full mb-2 text-lg text-gray-700 font-semibold"
-          >
-            Message
-          </label>
-          <textarea
-            id="message"
-            placeholder="Your Feedback"
-            className="input-style"
-            onChange={handleChange}
-          ></textarea>
+            <label
+              htmlFor="message"
+              className="text-left w-full mb-2 text-lg text-gray-700 font-semibold"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              placeholder="Your Feedback"
+              className="input-style"
+              onChange={handleChange}
+            ></textarea>
 
-          <button
-            type="submit"
-            className="btn-style bg-green-500 hover:bg-green-600"
-          >
-            Submit
-          </button>
-        </form>
-        <p className="mt-4 text-sm text-gray-600">
-          Thank you for your valuable feedback.
-        </p>
+            <button
+              type="submit"
+              className="btn-style bg-green-500 hover:bg-green-600"
+            >
+              Submit
+            </button>
+          </form>
+          <p className="mt-4 text-sm text-gray-600">
+            Thank you for your valuable feedback.
+          </p>
+        </div>
       </div>
-    </footer> // END: Change background color
+    </footer>
   );
 };
 
